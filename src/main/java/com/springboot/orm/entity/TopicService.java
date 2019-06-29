d0f5d1a69d41fc889d687230c36620566f47d3a3
sriramede9 <only4programming03@gmail.com>
2019-06-29 09:54:18 -0400

    added jpa to derby crud operations

--- src/main/java/com/springboot/orm/entity/TopicService.java
+++ src/main/java/com/springboot/orm/entity/TopicService.java
@@ -2,12 +2,19 @@ package com.springboot.orm.entity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.springboot.dao.TopicRepository;
 
 @Service
 public class TopicService {
+
+	@Autowired
+	private TopicRepository topicRepository;
 
 	List<Topics> topics = new ArrayList(
 			Arrays.asList(new Topics("spring", "springframework", "spring framework description"),
@@ -17,30 +24,62 @@ public class TopicService {
 			));
 
 	public List<Topics> getAll() {
-		return topics;
+		Iterator i = topicRepository.findAll().iterator();
+
+		List<Topics> topicslist = new ArrayList();
+		;
+
+		while (i.hasNext()) {
+			topicslist.add((Topics) i.next());
+		}
+
+		return topicslist;
 	}
 
 	// get one topic
 	public Topics getOne(String id) {
 
-		return topics.stream().filter(f -> f.getId().equals(id)).findFirst().get();
+		Topics findOne = null;
+		if (topicRepository.exists(id)) {
+			findOne = topicRepository.findOne(id);
+		}
+
+		return findOne;
 	}
 
 	public void updateTopic(String id, Topics t) {
+		Topics findOne = null;
+
 		Topics topic = topics.stream().filter(f -> f.getId().equals(id)).findFirst().get();
 
-		topic.setName(t.getName());
-		topic.setDescription(t.getDescription());
+		if (topicRepository.exists(id)) {
 
+			findOne = topicRepository.findOne(id);
+		}
+		;
+
+		findOne.setName(t.getName());
+		findOne.setDescription(t.getDescription());
+
+		topicRepository.save(findOne);
 	}
 
 	public void addTopic(Topics t) {
-		topics.add(t);
+
+		topicRepository.save(t);
+
+		// topics.add(t);
 	}
 
 	public void deleteTopic(String id) {
 		Topics topic = topics.stream().filter(f -> f.getId().equals(id)).findFirst().get();
 		topics.remove(topic);
+
+		if (topicRepository.exists(id)) {
+
+			topicRepository.delete(id);
+		}
+
 	}
 
 }
